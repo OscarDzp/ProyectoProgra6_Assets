5c86fef49201d19a3ba2bbe8980074f474839163
Oscar <ozumbado2000@gmail.com>
2024-03-08 08:55:09 -0600

    Semana 5 lista

--- ProyectoProgra6_Assets/Program.cs
+++ ProyectoProgra6_Assets/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ProyectoProgra6_Assets.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var CnnStrBuilder = new
+
+SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("CNNSTR"));
+
+string CnnStr = CnnStrBuilder.ConnectionString;
+
+builder.Services.AddDbContext<Progra6Proyecto2024Context>(options => options.UseSqlServer(CnnStr));
+
+CnnStrBuilder.Password = "progra6";
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
